e7c589de005b4e068976ec620ddb60e04e205804
Chendi Luo <chendi.luo@gmail.com>
2020-02-08 19:48:08 -0800

    added comments to pong

--- pong.py
+++ pong.py
@@ -3,9 +3,12 @@ import pygame.freetype
 import random
 pygame.init()
 
+# pygame variables
 w = pygame.display.set_mode([600,600])
 c = pygame.time.Clock()
 f = pygame.freetype.SysFont("Wingdings", 40)
+
+# game variables
 white = (255,255,255)
 pspeed = 10
 xspeed = random.choice([10, -10])
@@ -16,13 +19,16 @@ ball = pygame.Rect(300, 300, 10, 10)
 p1 = 0
 p2 = 0
 
+# main loop
 running = True
 while running:
+    # check for quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             break
     
+    # paddle movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         if player2.y > pspeed:
@@ -37,6 +43,7 @@ while running:
         if player.y < 550 - pspeed:
             player.y += pspeed
     
+    # ball movememt
     if (ball.y > 600 or ball.y < 0):
         yspeed *= -1
     if ball.x > 600:
@@ -56,6 +63,7 @@ while running:
     ball.x += xspeed
     ball.y += yspeed
     
+    # draw
     w.fill((0, 0, 0))
     pygame.draw.rect(w, white, player)
     pygame.draw.rect(w, white, player2)
